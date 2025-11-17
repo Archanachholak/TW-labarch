f8989b2df9dd78e68c4110241e903bd9cb37a02c
Charan <2024ucp1168@mnit.ac.in>
2025-11-17 15:09:11 +0530

    edit file

---
+++ dotproduct.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Hardcoded input vectors
+    int a[] = {1, 2, 3};
+    int b[] = {4, 5, 6};
+    int n = 3;   // size of vectors
+
+    int dot = 0;
+
+    for (int i = 0; i < n; i++) {
+        dot += a[i] * b[i];
+    }
+
+    cout << "Dot Product = " << dot << endl;
+
+    return 0;
+}
+
